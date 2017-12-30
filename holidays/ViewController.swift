f3df9ddecf6632da5c33f7ebd0a98438b504396c
MANUEL SOBERANIS <majesova@gmail.com>
2017-12-29 21:57:58 -0600

    Initial Commit

---
+++ holidays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  holidays
+//
+//  Created by MANUEL SOBERANIS on 29/12/17.
+//  Copyright © 2017 com.majesova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
