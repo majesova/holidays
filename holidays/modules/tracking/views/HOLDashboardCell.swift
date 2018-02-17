3d8cb26b2fa337d793d9b9fc4a95bb2dc0562861
MANUEL SOBERANIS <majesova@gmail.com>
2018-02-17 00:35:08 -0600

    Dashboard v1

---
+++ holidays/modules/tracking/views/HOLDashboardCell.swift
@@ -0,0 +1,14 @@
+//
+//  HOLDashboardCellCollectionViewCell.swift
+//  holidays
+//
+//  Created by MANUEL SOBERANIS on 16/02/18.
+//  Copyright © 2018 com.majesova. All rights reserved.
+//
+
+import UIKit
+
+class HOLDashboardCell: UICollectionViewCell {
+    
+    
+}
